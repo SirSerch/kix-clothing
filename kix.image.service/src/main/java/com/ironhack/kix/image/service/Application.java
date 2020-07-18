fa68606f19d696bb62677235415b3abfae426890
Sergio Santos <santoscamin@gmail.com>
2020-07-18 14:50:53 +0200

    Create project with all services

---
+++ kix.image.service/src/main/java/com/ironhack/kix/image/service/Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.kix.image.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ kix.product.service/src/test/java/com/ironhack/kix/product/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.kix.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

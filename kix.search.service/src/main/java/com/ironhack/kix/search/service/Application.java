037210888cb72b0fe4b56366b91dead3dbcd7f13
Sergio Santos <santoscamin@gmail.com>
2020-07-18 15:01:03 +0200

    Add feign client and add Log4j2

--- kix.search.service/src/main/java/com/ironhack/kix/search/service/Application.java
+++ kix.search.service/src/main/java/com/ironhack/kix/search/service/Application.java
@@ -2,8 +2,12 @@ package com.ironhack.kix.search.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
